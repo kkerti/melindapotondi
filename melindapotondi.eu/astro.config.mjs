b38d17489994957d4bbdb1d6dc20fee4eeea12cb
Kristóf Kerti <47832952+kkerti@users.noreply.github.com>
2025-10-11 23:26:50 +0200

    kk added cloudflare adapter

--- melindapotondi.eu/astro.config.mjs
+++ melindapotondi.eu/astro.config.mjs
@@ -2,16 +2,21 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   i18n: {
     locales: ["en", "hu"],
     defaultLocale: "hu",
     routing: {
         prefixDefaultLocale: false
     }
-  }
+  },
+
+  adapter: cloudflare()
 });
